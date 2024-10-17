288bba293137764bc58f207bb2c343447e97b556
Tuğçe Göksu <43906482+TugceKocak@users.noreply.github.com>
2024-10-17 23:06:03 +0300

    update

--- Password Manager GUI/main.py
+++ Password Manager GUI/main.py
@@ -2,22 +2,48 @@ from contextlib import nullcontext
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
 
 def save_data():
     web=web_entry.get()
     user=username_entry.get()
     passw=pw_entry.get()
+    new_data={
+        web:{
+            "email":user,
+            "password":passw,
+        }
+    }
     if web=="" or user=="" or passw=="":
         messagebox.showinfo("Oops","Please don't leave any fields empty!",icon='warning')
     else:
         is_ok=messagebox.askokcancel(title=web, message="Is it ok to save data?")
         if is_ok:
-            new_content=f"{web} | {user} | {passw}\n"
-            with open("data.txt","a") as file:
-                file.write(new_content)
-                file.close()
-            web_entry.delete(0,END)
-            pw_entry.delete(0,END)
+            try:
+                with open("data.json","r") as file:
+                    data=json.load(file)
+                    data.update(new_data)
+            except FileNotFoundError:
+                with open("data.json", "w") as file:
+                    json.dump(new_data, file, indent=4)
+            else:
+                with open("data.json","w") as file:
+                    json.dump(data,file,indent=4)
+            finally:
+                web_entry.delete(0,END)
+                pw_entry.delete(0,END)
+def search():
+    try:
+        with open("data.json","r") as file:
+            data=json.load(open("data.json"))
+            mail=f"{data[web_entry.get()].get('email')}"
+            password=f"{data[web_entry.get()].get('password')}"
+            messagebox.showinfo(title=web_entry.get(), message=f"Email: {mail}\nPassword: {password}")
+    except FileNotFoundError:
+        messagebox.showwarning(title="Error", message="No Data File found.")
+    except KeyError:
+        print("You dont have account!")
+
 def generate_password(password=None):
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -60,9 +86,12 @@ username_label.grid(column=0, row=2)
 password_label=Label(window, text='Password:')
 password_label.grid(column=0, row=3)
 
-web_entry= Entry(width=55)
-web_entry.grid(column=1, row=1, columnspan=2)
+web_entry= Entry(width=35)
+web_entry.grid(column=1, row=1)
 web_entry.focus()
+
+search_button=Button(text="Search", width=15, command=search)
+search_button.grid(column=2, row=1)
 
 username_entry= Entry(width=55)
 username_entry.grid(column=1, row=2, columnspan=2)
